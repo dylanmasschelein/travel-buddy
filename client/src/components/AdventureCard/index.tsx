dc1e893ebc9b0dea68598060446d96e9c34a1895
dylanmasschelein <dylanmasschelein@gmail.comclear>
2021-07-30 08:19:55 -0700

    Create initial profile drop down menu, build scaffolding for AdventureCards

---
+++ client/src/components/AdventureCard/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import "./AdventureCard.scss";
+
+const AdventureCard: FC = () => {
+  return (
+    <div>
+      <h2>Title</h2>
+      <p>Country</p>
+      <p>Length of stay</p>
+      <p>img</p>
+    </div>
+  );
+};
+export default AdventureCard;
--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -5,6 +5,8 @@ import Blog from "../../components/Blog";
 import AdventureMap from "../../components/AdventureMap";
 import { useEffect } from "react";
 import { loadMapApi } from "../../utils/google-maps-config";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 interface UserProps {
   user: { name: string; email: string; id: number };
@@ -12,21 +14,42 @@ interface UserProps {
 
 const ProfilePage: FC<UserProps> = ({ user }) => {
   const [scriptLoaded, setScriptLoaded] = useState(false);
+  const [tripCoords, setTripCoords] = useState(null);
+  const [profNav, setProfNav] = useState(false);
 
   useEffect(() => {
     const googleMapScript = loadMapApi();
     googleMapScript.addEventListener("load", () => {
-      setScriptLoaded(true);
+      setScriptLoaded(false); // change to true to display map
     });
   }, []);
 
   return (
-    <div>
-      <Link to='/profile/blog'>Blog</Link>
-      <Link to='/profile/recommendations'>Recommendations</Link>
-      <Link to='/profile/photos'>Photos</Link>
-      <Link to='/profile/map'>Adventure map</Link>
-      <Link to='/profile/bucketlist'>Bucket list</Link>
+    <div className='profile'>
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={() => setProfNav(!profNav)}
+        className='profile__bars'
+      />
+      {profNav && (
+        <nav className='profile__nav'>
+          <Link to='/profile/blog' className='profile__link'>
+            Blog
+          </Link>
+          <Link to='/profile/recommendations' className='profile__link'>
+            Recommendations
+          </Link>
+          <Link to='/profile/photos' className='profile__link'>
+            Photos
+          </Link>
+          <Link to='/profile/map' className='profile__link'>
+            Adventure map
+          </Link>
+          <Link to='/profile/bucketlist' className='profile__link'>
+            Bucket list
+          </Link>
+        </nav>
+      )}
       {scriptLoaded && (
         <AdventureMap
           mapType={google.maps.MapTypeId.ROADMAP}
